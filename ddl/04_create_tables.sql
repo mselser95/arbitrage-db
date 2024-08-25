c783a2e59f28f2d45a684eb393fcfd4a908b66e8
Matias Selser <mati.selser@hotmail.com>
2024-08-25 17:18:29 -0300

    token discovery, adjustmetns

--- ddl/04_create_tables.sql
+++ ddl/04_create_tables.sql
@@ -1,18 +1,24 @@
 CREATE TABLE arbitrage_schema.erc20_tokens (
-                                               id VARCHAR(42) PRIMARY KEY,
-                                               discovery_block BIGINT NOT NULL,
-                                               name VARCHAR(255) NOT NULL,
-                                               decimals INTEGER NOT NULL,
-                                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                               chain_id BIGINT NOT NULL
+    id UUID PRIMARY KEY,
+    address_lower VARCHAR(42),
+    discovery_block BIGINT NOT NULL,
+    name VARCHAR(255) ,
+    decimals INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    chain_id BIGINT NOT NULL
 );
 
 CREATE TABLE arbitrage_schema.markets (
-                                          id VARCHAR(100) PRIMARY KEY,
-                                          provider BIGINT NOT NULL,
-                                          input_token VARCHAR(42) REFERENCES arbitrage_schema.erc20_tokens(id),
-                                          output_token VARCHAR(42) REFERENCES arbitrage_schema.erc20_tokens(id),
-                                          pool_fee BIGINT,
-                                          pool_identifier VARCHAR(40),
-                                          is_stable BOOLEAN NOT NULL
+    id UUID PRIMARY KEY,
+    provider BIGINT NOT NULL,
+    input_token UUID REFERENCES arbitrage_schema.erc20_tokens(id),
+    output_token UUID REFERENCES arbitrage_schema.erc20_tokens(id),
+    pool_fee BIGINT,
+    pool_identifier VARCHAR(40),
+    is_stable BOOLEAN
+);
+
+CREATE TABLE last_block_processed (
+    chain_id INT PRIMARY KEY,
+    block_number BIGINT NOT NULL
 );
